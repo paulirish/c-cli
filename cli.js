e27c2d3ee6e2efc48850ca64b6c36f98d5989090
Paul Irish <paul.irish@gmail.com>
2017-03-26 15:22:35 -0700

    c-cli.

--- cli.js
+++ cli.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
+
 const emphasize = require('emphasize');
 
-const doc = fs.readFileSync('./package.json', 'utf8');
+const filePath = process.argv[2];
+if (!filePath) {
+    console.error('USAGE: c filename.ext');
+    process.exit(1);
+}
+const resolvedPath = path.resolve(process.cwd(), filePath);
+
+const doc = fs.readFileSync(resolvedPath, 'utf8');
 const output = emphasize.highlightAuto(doc).value;
 
 process.stdout.write(output);
-
